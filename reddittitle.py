07c1a0b3fa5ec3e84a4c3c660965756dcdcdcafd
blha303 <stevensmith.ome@gmail.com>
2017-02-13 19:56:51 +0800

    Initial

---
+++ reddittitle.py
@@ -0,0 +1,32 @@
+from flask import *
+import praw,prawcore
+
+app = Flask(__name__)
+app.config.from_pyfile("settings.cfg")
+reddit = praw.Reddit(site_name="reddittitle")
+
+@app.route("/")
+def index():
+    if not "code" in session:
+        return redirect("/login")
+    try:
+        r = praw.Reddit(user_agent="Praw user instance", site_name="reddittitle")
+        _ = list(r.user.moderator_subreddits())
+        return str(_)
+    except praw.exceptions.APIException:
+        return redirect("/login")
+
+@app.route("/login")
+def login():
+    return redirect(reddit.auth.url(["identity", "modconfig", "mysubreddits"], "state", "temporary", False))
+
+@app.route("/postlogin")
+def postlogin():
+    session["code"] = request.args.get("code")
+    return redirect("/")
+
+def main():
+    app.run(port=5000, debug=True, host="0.0.0.0")
+
+if __name__ == "__main__":
+    main()
